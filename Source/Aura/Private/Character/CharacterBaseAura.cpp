5784cf127483e70731c32c7c487fe9322d32c155
ArtosDave <david.ruiz@ou.ac.uk>
2024-06-25 18:56:58 +0100

    Added both Skelettal meshes

--- Source/Aura/Private/Character/CharacterBaseAura.cpp
+++ Source/Aura/Private/Character/CharacterBaseAura.cpp
@@ -7,6 +7,9 @@ ACharacterBaseAura::ACharacterBaseAura()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void ACharacterBaseAura::BeginPlay()
--- Source/Aura/Public/Character/CharacterBaseAura.h
+++ Source/Aura/Public/Character/CharacterBaseAura.h
@@ -17,4 +17,11 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, Category="Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
+
 };
+
+
+
+

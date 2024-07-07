ae6e962000f832e9667743a1f7f42a06421618a1
ArtosDave <david.ruiz@ou.ac.uk>
2024-07-07 16:18:24 +0100

    11 Game Mode

---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright bolasGrandes
+
+
+#include "Game/AuraGameModeBase.h"
+
--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -44,16 +44,16 @@ EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &
 void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 {
 	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
-	const  FRotator Rotation = GetControlRotation();
+	const FRotator Rotation = GetControlRotation();
 	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
 
 	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
 	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 
 	if(APawn* ControlledPawn = GetPawn<APawn>())
+		
 	{
-
 		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
-		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.X);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
 	}
 }
